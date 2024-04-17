68f395c52c7c08d311c74686229e32142f22487c
aliismayilov <t232986@gmail.com>
2024-04-17 18:02:51 +0400

    Project started,Settings completed,Game desk finished

---
+++ xo.js
@@ -0,0 +1,27 @@
+function showSettings() {
+    document.getElementById("start").style.display = "none"
+    document.getElementById("theme").style.display = "flex"
+}
+function darkTheme() {
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+    for (let i = 0; i < document.querySelectorAll("div#game > div").length; i++) {
+        document.querySelectorAll("div#game > div")[i].style.borderColor="white"
+    }
+}
+function lightTheme() {
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+}
+function backToMenu() {
+    document.getElementById("start").style.display = "flex"
+    document.getElementById("theme").style.display = "none"
+    document.getElementById("game").style.display = "none"
+    document.getElementById("back").style.display = "none"
+
+}
+function startGame() {
+    document.getElementById("start").style.display = "none"
+    document.getElementById("game").style.display = "grid"
+    document.getElementById("back").style.display = "block"
+}
