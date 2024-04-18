88774ea719d95119919eedcd4494aab098305083
aliismayilov <t232986@gmail.com>
2024-04-18 16:50:33 +0400

    Writing added,winner function is not working

--- xo.js
+++ xo.js
@@ -6,14 +6,14 @@ function darkTheme() {
     document.body.style.backgroundColor = "black"
     document.body.style.color = "white"
     for (let i = 0; i < document.querySelectorAll("div#game > div").length; i++) {
-        document.querySelectorAll("div#game > div")[i].style.borderColor="white"
+        document.querySelectorAll("div#game > div")[i].style.borderColor = "white"
     }
 }
 function lightTheme() {
     document.body.style.backgroundColor = "white"
     document.body.style.color = "black"
     for (let i = 0; i < document.querySelectorAll("div#game > div").length; i++) {
-        document.querySelectorAll("div#game > div")[i].style.borderColor="black"
+        document.querySelectorAll("div#game > div")[i].style.borderColor = "black"
     }
 }
 function backToMenu() {
@@ -28,3 +28,25 @@ function startGame() {
     document.getElementById("game").style.display = "grid"
     document.getElementById("back").style.display = "block"
 }
+let turn = "x"
+function writeX(x) {
+    for (let i = 0; i < document.querySelectorAll("div#game > div").length; i++) {
+        if (turn == "x") {
+            document.querySelectorAll("div#game > div")[x - 1].innerHTML = `<i class="fa-solid fa-xmark"></i>`
+            turn = "o"
+        }
+        else {
+            document.querySelectorAll("div#game > div")[x - 1].innerHTML = `<i class="fa-regular fa-circle"></i>`
+            turn = "x"
+        }
+    }
+}
+
+function findWinner() {
+    if ((document.querySelectorAll("div#game > div")[0].innerHTML == document.querySelectorAll("div#game > div")[1].innerHTML == document.querySelectorAll("div#game > div")[2].innerHTML) || (document.querySelectorAll("div#game > div")[0].innerHTML == document.querySelectorAll("div#game > div")[4].innerHTML == document.querySelectorAll("div#game > div")[8].innerHTML) || (document.querySelectorAll("div#game > div")[0].innerHTML == document.querySelectorAll("div#game > div")[3].innerHTML == document.querySelectorAll("div#game > div")[6].innerHTML)) {
+        document.getElementById("game").innerHTML = `<h1>Winner is X player</h1>`
+        console.log(x);
+    }
+}
+findWinner()
+console.log(document.querySelectorAll("div#game > div")[0]);
